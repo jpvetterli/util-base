058bd4f56834077035aa622a98f0e5f1354bfbef
Jean-Paul Vetterli <vetterli@gmail.com>
2013-06-07 17:48:54 +0200

    Harden and document test case.

--- src/test/java/ch/agent/util/res/LoggerTest.java
+++ src/test/java/ch/agent/util/res/LoggerTest.java
@@ -9,53 +9,72 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import ch.agent.util.file.TextFile;
 
 public class LoggerTest {
 
-	protected final Log log = LogFactory.getLog(LoggerTest.class);
-	protected static File logFile;
+	private static int toStringCount;
 	
-	@Before
-	public void setUp() throws Exception {
-		// initialize logger using code because we need a temporary file
-		if (logFile != null)
-			return;
-		logFile = File.createTempFile("log-", ".log");
-		FileAppender a = new FileAppender();
-		a.setFile(logFile.getAbsolutePath());
-		a.setLayout(new PatternLayout("[%p] %m%n"));
-		//a.setThreshold(Level.DEBUG);
-		a.activateOptions();
-		Logger.getRootLogger().addAppender(a);
+	private class TestMessage extends LazyMessage {
+		public TestMessage(String message, Object... args) {
+			super(message, args);
+		}
+		@Override
+		public String toString() {
+			toStringCount++;
+			return super.toString();
+		}
 	}
 	
-	@After
-	public void tearDown() {
-		if (logFile != null)
-			logFile.delete();
-	}
-
+	/**
+	 * This tests support for the java.text.MessageFormat syntax. This format is
+	 * very useful in multilingual contexts, where an important requirement is
+	 * to modify the order of parameters when translating message texts.
+	 * <p>
+	 * SLF4J does not support this while JCL does.
+	 * <p>
+	 * The test in this project assumes JCL with log4j. There is a variation of
+	 * this test in project util-web which uses the combination JCL-over-SLF4J
+	 * with logback. Testing different combinations in different projects makes
+	 * life easier because the mere presence of some jars affect how loggers
+	 * behave.
+	 * <p>
+	 * The 3d assertion tests that parameter are handled lazily by counting
+	 * calls of {@link TestMessage#toString}.
+	 */
 	@Test
 	public void testSimpleMessageWithTwoArgsReversed() {
+		File logFile = null;
 		try {
-			/*
-			 * Test support for the java.text.MessageFormat syntax.
-			 * Note (7.6.2013): SLF4J does not support it. So, use JCL+log4j.
-			 */
-			log.info(new LazyMessage("#2={1} #1={0}", Double.NaN, "foo"));
+			logFile = File.createTempFile("log-", ".latog");
+			Log log = setUpLog4J(logFile);
+			log.info(new TestMessage("#2={1} #1={0}", Double.NaN, "foo"));
+			log.debug(new TestMessage("#2={1} #1={0}", Double.NaN, "foo"));
 			TextFile f = new TextFile();
 			List<String> lines = f.read(logFile.getAbsolutePath());
 			assertEquals(1, lines.size());
 			assertEquals("[INFO] #2=foo #1=NaN", lines.get(0));
+			assertEquals(1, toStringCount); // see note above 
 		} catch (Exception e) {
 			fail("unexpected exception");
+		} finally {
+			logFile.delete();
 		}
 	}
+	
+	private Log setUpLog4J(File f) throws Exception {
+		FileAppender a = new FileAppender();
+		a.setFile(f.getAbsolutePath());
+		a.setLayout(new PatternLayout("[%p] %m%n"));
+		a.setThreshold(Level.INFO);
+		a.activateOptions();
+		Logger.getRootLogger().addAppender(a);
+		return LogFactory.getLog(LoggerTest.class);
+	}
+
 }
