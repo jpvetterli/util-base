f6df1fc2c1cba588b808b7a5686465019770d73f
Jean-Paul Vetterli <vetterli@gmail.com>
2017-09-19 21:55:44 +0200

    Implement severity level to support tuning the default logger.

--- src/main/java/ch/agent/util/logging/DefaultLoggerBridge.java
+++ src/main/java/ch/agent/util/logging/DefaultLoggerBridge.java
@@ -1,102 +1,144 @@
 package ch.agent.util.logging;
 
+
 import ch.agent.util.base.LazyString;
 
 /**
- * Default logger bridge. Always prints all messages to standard error.
- * Messages are prefixed with a capital letter indicating the logging level.
+ * Default logger bridge. Prints messages to standard error. Messages are
+ * prefixed with a capital letter indicating the logging severity. A severity
+ * level is set when creating the logger bridge. Logging of messages less severe
+ * than this level is disabled. The severity levels 
+ * are 
+ * <ul>
+ * <li>0 (or less) log all messages</li>
+ * <li>1 log "trace", "debug", "info", "warn" and "error" messages</li>
+ * <li>2 log "debug", "info", "warn" and "error" messages</li>
+ * <li>3 log "info", "warn" and "error" messages</li>
+ * <li>4 log only "warn" and "error" messages</li>
+ * <li>5 log only "error" messages</li>
+ * <li>6 (or more) do not log any message</li>
+ * </ul>
  */
 public class DefaultLoggerBridge implements LoggerBridge {
 
+	private static final int TRACE = 1;
+	private static final int DEBUG = 2;
+	private static final int INFO = 3;
+	private static final int WARN = 4;
+	private static final int ERROR = 5;
 	
-	public DefaultLoggerBridge() {
+	private final int severity;
+	
+	/**
+	 * Constructor. Messages less severe than the argument will be ignored.
+	 * 
+	 * @param severity
+	 *            a value between 0 and 6
+	 */
+	public DefaultLoggerBridge(int severity) {
+		this.severity = severity;
 	}
+	
 
 	@Override
 	public boolean isTraceEnabled() {
-		return true;
+		return severity <= TRACE;
 	}
 
 	@Override
 	public boolean isDebugEnabled() {
-		return true;
+		return severity <= DEBUG;
 	}
 
 	@Override
 	public boolean isInfoEnabled() {
-		return true;
+		return severity <= INFO;
 	}
 
 	@Override
 	public boolean isWarnEnabled() {
-		return true;
+		return severity <= WARN;
 	}
 
 	@Override
 	public boolean isErrorEnabled() {
-		return true;
+		return severity <= ERROR;
 	}
 	
 	@Override
 	public void trace(LazyString msg) {
-		System.err.println("T " + msg.toString());
+		if (isTraceEnabled())
+			System.err.println("T " + msg.toString());
 	}
 
 	@Override
 	public void trace(String msg) {
-		System.err.println("T " + msg);
+		if (isTraceEnabled())
+			System.err.println("T " + msg);
 	}
 
 	@Override
 	public void debug(LazyString msg) {
-		System.err.println("D " + msg.toString());
+		if (isDebugEnabled())
+			System.err.println("D " + msg.toString());
 	}
 
 	@Override
 	public void debug(String msg) {
-		System.err.println("D " + msg);
+		if (isDebugEnabled())
+			System.err.println("D " + msg);
 	}
 
 	@Override
 	public void info(LazyString msg) {
-		System.err.println("I " + msg.toString());
+		if (isInfoEnabled())
+			System.err.println("I " + msg.toString());
 	}
 
 	@Override
 	public void info(String msg) {
-		System.err.println("I " + msg);
+		if (isInfoEnabled())
+			System.err.println("I " + msg);
 	}
 
 	@Override
 	public void warn(LazyString msg) {
-		System.err.println("W " + msg.toString());
+		if (isWarnEnabled())
+			System.err.println("W " + msg.toString());
 	}
 
 	@Override
 	public void warn(String msg) {
-		System.err.println("W " + msg);
+		if (isWarnEnabled())
+			System.err.println("W " + msg);
 	}
 
 	@Override
 	public void error(LazyString msg) {
-		System.err.println("E " + msg.toString());
+		if (isErrorEnabled())
+			System.err.println("E " + msg.toString());
 	}
 
 	@Override
 	public void error(String msg) {
-		System.err.println("E " + msg);
+		if (isErrorEnabled())
+			System.err.println("E " + msg);
 	}
 
 	@Override
 	public void error(LazyString msg, Throwable t) {
-		System.err.println("E " + msg.toString());
-		t.printStackTrace(System.err);
+		if (isErrorEnabled()) {
+			System.err.println("E " + msg.toString());
+			t.printStackTrace(System.err);
+		}
 	}
 
 	@Override
 	public void error(String msg, Throwable t) {
-		System.err.println("E " + msg);
-		t.printStackTrace(System.err);
+		if (isErrorEnabled()) {
+			System.err.println("E " + msg);
+			t.printStackTrace(System.err);
+		}
 	}
 
 }
--- src/main/java/ch/agent/util/logging/DefaultLoggerBridgeFactory.java
+++ src/main/java/ch/agent/util/logging/DefaultLoggerBridgeFactory.java
@@ -1,13 +1,21 @@
 package ch.agent.util.logging;
 
-
 /**
  * The default logger factory provides access to the default logger bridge.
  */
 public final class DefaultLoggerBridgeFactory implements LoggerBridgeFactory {
 	// final class because nothing to subclass
 
+	public static final String DEFAULT_LOGGER_BRIDGE_SEVERITY = "DefaultLoggerBridgeSeverity";
 	private static DefaultLoggerBridgeFactory factory = new DefaultLoggerBridgeFactory();
+	private static int severity = 0; // log everything
+
+	static{
+		String severityProperty = System.getProperty(DEFAULT_LOGGER_BRIDGE_SEVERITY);
+		if (severityProperty != null)
+			try {severity = Integer.parseInt(severityProperty);} catch (Throwable t) {}
+		factory = new DefaultLoggerBridgeFactory();
+	}
 	
 	/**
 	 * Get the factory singleton.
@@ -23,12 +31,12 @@ public final class DefaultLoggerBridgeFactory implements LoggerBridgeFactory {
 
 	@Override
 	public LoggerBridge getLogger(String name) {
-		return new DefaultLoggerBridge();
+		return new DefaultLoggerBridge(severity);
 	}
 
 	@Override
 	public LoggerBridge getLogger(Class<?> klass) {
-		return new DefaultLoggerBridge();
+		return new DefaultLoggerBridge(severity);
 	}
 	
 }
